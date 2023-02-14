bd2be6a29235fc23dce3c89dc310d1b8071751d7
jeongwoo0427 <jwk90226482@gmail.com>
2023-02-14 16:51:16 +0900

    feat: receipt 결과데이터 송신

--- app/controller/splat_banner_controller.js
+++ app/controller/splat_banner_controller.js
@@ -3,8 +3,8 @@ const SplatBannerReceiptModel = require('../models/splat_banner_receipt_model');
 module.exports = {
     insertReceipt : async (req,res,next) =>{
         try{
-            await SplatBannerReceiptModel.create(req.body);
-            return res.send('result');
+            const result = await SplatBannerReceiptModel.create(req.body);
+            return res.send(result);
         }catch(err){
             next(err);
         }
--- izapi.js
+++ izapi.js
@@ -8,7 +8,7 @@ const app = express();
 
 const port = 33;
 
-sequelize.sync({force:true}).then(()=>{
+sequelize.sync({force:false}).then(()=>{
     console.log('데이터베이스 연결 성공');
 }).catch((err)=>{
     console.error('sequelize 오류)'+err);
