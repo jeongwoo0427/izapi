f1d8b0a15aeb6c37ca2fddfc37066ead510aaf76
jeongwoo0427 <jwk90226482@gmail.com>
2023-11-15 23:30:51 +0900

    feat : socketio html 샘플과 연동 성공

--- socket_server.js
+++ socket_server.js
@@ -1,18 +1,33 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const app = express();
 
 const port = 34;
-
-io.on('connection', (socket) => {
-    console.log('some user connected!');
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    })
+const server = app.listen(port, function() {
+    console.log('Socket Server is listening on port',port);
 });
 
+const SocketIO = require('socket.io');
+const io = SocketIO(server, {path: '/socket.io'});
 
-http.listen(port, () =>{
-    console.log('Socket Server is listening on port',port);
-})
+io.on('connection', function (socket) {
+    console.log(socket.id, ' connected...');
+    
+    // broadcasting a entering message to everyone who is in the chatroom
+    io.emit('msg', `${socket.id} has entered the chatroom.`);
+  
+  	// message receives
+    socket.on('msg', function (data) {
+        console.log(socket.id,': ', data);
+        // broadcasting a message to everyone except for the sender
+        socket.broadcast.emit('msg', `${socket.id}: ${data}`);
+    });
+
+    // user connection lost
+    socket.on('disconnect', function (data) {
+        io.emit('msg', `${socket.id} has left the chatroom.`);
+    });
+});
+
+app.get('/chat', function(req, res) {
+    res.sendFile(__dirname + '/html/socket_client.html');
+});
